22682c42e0c0c570a319a5708d9d287eccbd0178
Edu-Paz <eduardopazcarv@gmail.com>
2026-02-02 12:22:12 -0300

    Refactor: change the names of variables and exercises

---
+++ recursion_exercises/src/exercise_01/ReverseArrayPrinter.java
@@ -0,0 +1,19 @@
+package exercise_01;
+
+public class ReverseArrayPrinter {
+
+	public void printArray(int[] array, int n) {
+		if (n < 0) {
+			return;
+		}
+			System.out.println(array[n]);
+			printArray(array, n - 1);
+	}
+	public static void main(String[] args) {
+		ReverseArrayPrinter ex = new ReverseArrayPrinter();
+		int [] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		
+		ex.printArray(array, (array.length - 1));
+	}
+
+}
---
+++ recursion_exercises/src/exercise_02/ArraySum.java
@@ -0,0 +1,31 @@
+package exercise_02;
+
+public class ArraySum {
+
+	public double somaArray(double[][] array, int x, int y) {
+		if(x >= array.length) {
+			return 0;
+		}
+		
+		int nextX = x;
+		int nextY = y + 1;
+		
+		if (nextY >= array[x].length) {
+			nextX = x + 1;
+			nextY = 0;
+		}
+		
+		return array[x][y] + somaArray(array, nextX, nextY);
+	}
+	public static void main(String[] args) {
+		ArraySum ex = new ArraySum();
+        double[][] data = {
+            {1.5, 2.5},
+            {3.0, 4.0}
+        };
+
+        double total = ex.somaArray(data, 0, 0);
+        System.out.println("The total sum is: " + total);
+	}
+
+}
